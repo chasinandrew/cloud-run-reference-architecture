cb800ef9cfa666dd23f512eedab250df0b9a3656
Andrew Chasin <andrewchasin@google.com>
2023-04-10 13:19:58 -0400

    move terraform cloud implemetation

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,13 +6,14 @@ terraform {
       version = ">= 3.9.0, < 5.0.0"
     }
   }
-}
-cloud {
-  organization = "google-org-testing"
-  workspaces {
-    name = "cloud-run-reference-architecture"
+  cloud {
+    organization = "google-org-testing"
+    workspaces {
+      name = "cloud-run-reference-architecture"
   }
 }
+}
+
 # provider "google" {
 #   project     = "arched-inkwell-368821"
 #   credentials = "/Users/andrewchasin/Documents/service-account-keys/new-demo.json"
