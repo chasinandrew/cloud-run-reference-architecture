4c66717d9bd46ed603118e39752d36931ae8e89a
Andrew Chasin <andrewchasin@google.com>
2023-06-23 08:14:24 -0400

    changes to deployment yaml for env based deployments

--- terraform/dev.tfvars
+++
@@ -1,5 +0,0 @@
-project_id                    = "arched-inkwell-368821"
-region                        = "us-east4"
-frontend_container_image      = "us-east4-docker.pkg.dev/arched-inkwell-368821/reference-architectures/backend:latest"
-frontend_service_name         = "pods"
-database_password_secret_name = "DB_PASS"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,16 +9,11 @@ variable "region" {
   description = "The region to create GCP resources within."
 }
 
-variable "frontend_container_image" {
-  type        = string
-  description = "The container image of the frontend Cloud Run instance."
-}
 
 variable "frontend_service_name" {
   type        = string
   description = "The service name of the frontend Cloud Run instance."
 }
-
 
 variable "database_password_secret_name" {
   type        = string
