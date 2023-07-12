983d21567351eb95467b56227f10af818e33c661
Andrew Chasin <andrewchasin@google.com>
2023-07-12 14:27:30 -0400

    get rid of data source

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,9 +2,9 @@ data "google_project" "project" {
   project_id = var.project_id
 }
 
-data "google_cloud_run_service" "container" {
-  project  = var.project_id
-  name     = var.frontend_service_name
-  location = var.region
-}
+# data "google_cloud_run_service" "container" {
+#   project  = var.project_id
+#   name     = var.frontend_service_name
+#   location = var.region
+# }
 
