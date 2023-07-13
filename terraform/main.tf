a2f8cc5fda1e6b3c4cd7ea0054e09ea29535bfd3
Andrew Chasin <andrewchasin@google.com>
2023-07-13 07:39:21 -0400

    remove cyclic dependency

--- terraform/main.tf
+++ terraform/main.tf
@@ -85,13 +85,8 @@ resource "google_cloud_run_service_iam_member" "noauth" {
 }
 
 resource "random_integer" "sneg_id" {
-  min = 1
+  min = 5
   max = 50
-  lifecycle {
-    replace_triggered_by = [
-      google_compute_region_network_endpoint_group.cloudrun_sneg.id
-    ]
-  }
 }
 
 resource "google_compute_region_network_endpoint_group" "cloudrun_sneg" {
