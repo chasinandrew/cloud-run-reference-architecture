ba55b8214de0276f1696fe13a84a8fd21d01977c
Andrew Chasin <andrewchasin@google.com>
2023-07-14 17:21:59 -0400

    adding dns A record

--- terraform/main.tf
+++ terraform/main.tf
@@ -194,3 +194,15 @@ module "secret-manager" {
     },
   ]
 } 
+
+resource "google_dns_record_set" "a" {
+  name         = "backend.${google_dns_managed_zone.prod.dns_name}"
+  managed_zone = google_dns_managed_zone.prod.name
+  type         = "A"
+  ttl          = 300
+}
+
+resource "google_dns_managed_zone" "prod" {
+  name     = "prod-zone"
+  dns_name = var.partially_qualified_domain_name
+}
