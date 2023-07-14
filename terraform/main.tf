caca9a3a279fa932f94c3ab52127eb0ddd84fa75
Andrew Chasin <andrewchasin@google.com>
2023-07-14 17:23:39 -0400

    test for cloud run ignore_changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -195,14 +195,16 @@ module "secret-manager" {
   ]
 } 
 
-resource "google_dns_record_set" "a" {
-  name         = "backend.${google_dns_managed_zone.prod.dns_name}"
-  managed_zone = google_dns_managed_zone.prod.name
-  type         = "A"
-  ttl          = 300
-}
+# resource "google_dns_record_set" "a" {
+#   name         = "backend.${google_dns_managed_zone.prod.dns_name}"
+#   managed_zone = google_dns_managed_zone.prod.name
+#   type         = "A"
+#   ttl          = 300
 
-resource "google_dns_managed_zone" "prod" {
-  name     = "prod-zone"
-  dns_name = var.partially_qualified_domain_name
-}
+#   rrdatas = ["8.8.8.8"]
+# }
+
+# resource "google_dns_managed_zone" "prod" {
+#   name     = "prod-zone"
+#   dns_name = "prod.mydomain.com."
+# }
