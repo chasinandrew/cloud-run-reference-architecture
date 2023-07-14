0297f71af1b6c1d36c8e4178d88c5d49ffb61c0d
Andrew Chasin <andrewchasin@google.com>
2023-07-14 18:04:29 -0400

    see if anything changes with cloudrun

--- terraform/main.tf
+++ terraform/main.tf
@@ -196,7 +196,7 @@ module "secret-manager" {
 } 
 
 # resource "google_dns_record_set" "a" {
-#   name         = "backend.${google_dns_managed_zone.prod.dns_name}"#
+#   name         = "backend.${google_dns_managed_zone.prod.dns_name}"##
 #   managed_zone = google_dns_managed_zone.prod.name
 #   type         = "A"
 #   ttl          = 300
